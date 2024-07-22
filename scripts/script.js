2405e1eb27dd196f08f6648ecb130e46502c4f76
Naetffy <mateo.forero-f@mail.escuelaing.edu.co>
2024-07-22 00:29:47 -0500

    0.2.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,47 @@
+import { User } from '../api/lib/user.js';
 // scripts/script.js
 $(document).ready(function() {
-    const userInfo = `
-        <p>Name: ${user.name}</p>
-        <p>Email: ${user.email}</p>
-        <h3>Subjects:</h3>
-        <ul>
-            ${user.subjects.map(subject => `
-                <li>
-                    ${subject.name} - Final Grade: ${subject.getFinalGrade()}
-                    <ul>
-                        ${subject.grades.map(grade => `
-                            <li>Grade: ${grade[0]}, Percentage: ${grade[1]}</li>
-                        `).join('')}
-                    </ul>
-                </li>
-            `).join('')}
-        </ul>
-        <h3>Tasks:</h3>
-        <ul>
-            ${Object.keys(user.calendary.tasks).map(date => `
-                <li>
-                    ${date}
-                    <ul>
-                        ${user.calendary.tasks[date].map(task => `
-                            <li>${task.name} - Status: ${task.status} - Grade: ${task.grade || 'N/A'}</li>
-                        `).join('')}
-                    </ul>
-                </li>
-            `).join('')}
-        </ul>
-    `;
+    let user = null;
 
-    $('#TemporaryDiv').html(userInfo);
+    const userData = sessionStorage.getItem('user');
+    if (userData) {
+        user = User.fromJSON(userData);
+    }
+
+    if (user) {
+        const userInfo = `
+            <p>Name: ${user.name}</p>
+            <p>Email: ${user.email}</p>
+            <h3>Subjects:</h3>
+            <ul>
+                ${user.subjects.map(subject => `
+                    <li>
+                        ${subject.name} - Final Grade: ${subject.getFinalGrade()}
+                        <ul>
+                            ${subject.grades.map(grade => `
+                                <li>Grade: ${grade[0]}, Percentage: ${grade[1]}</li>
+                            `).join('')}
+                        </ul>
+                    </li>
+                `).join('')}
+            </ul>
+            <h3>Tasks:</h3>
+            <ul>
+                ${Object.keys(user.calendary.tasks).map(date => `
+                    <li>
+                        ${date}
+                        <ul>
+                            ${user.calendary.tasks[date].map(task => `
+                                <li>${task.name} - Status: ${task.status} - Grade: ${task.grade || 'N/A'}</li>
+                            `).join('')}
+                        </ul>
+                    </li>
+                `).join('')}
+            </ul>
+        `;
+
+        $('#TemporaryDiv').html(userInfo);
+    } else {
+        $('#TemporaryDiv').html('<p>No user data found.</p>');
+    }
 });
