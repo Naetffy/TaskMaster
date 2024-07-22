90bf96158559275ae8382b58b68d79faca5bbc41
Camila Torres <andrea.torres-g@mail.escuelaing.edu.co>
2024-07-22 12:25:26 -0500

    0.0.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,18 @@
+//Agregar el mensaje de bienvenida con el nombre de usuario registrado
+function addUserName(){
+    const userJson = sessionStorage.getItem('user');
+    const user = JSON.parse(userJson);
+    const newSpan = `<span>Welcome back, ${user.name}</span>`;
+    $("#user-container").prepend(newSpan);
+}
 
-document.addEventListener("DOMContentLoaded", function() {
+
+$(document).ready(function() {
     fetch('../components/navbar.html')
         .then(response => response.text())
         .then(data => {
-            console.log("prueba");
             document.getElementById('navbar-placeholder').innerHTML = data;
+            addUserName();
         })
         .catch(error => console.error('Error loading the navbar:', error));
 });
